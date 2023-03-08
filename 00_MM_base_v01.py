adc087c8ea49bca39e829c44e35022ad42876b7b
cargan3554 <90802528+cargan3554@users.noreply.github.com>
2023-03-08 14:50:00 +1300

    Add files via upload

--- 00_MM_base_v01.py
+++ 00_MM_base_v01.py
@@ -1,19 +1,5 @@
 # functions go here
-# Checks user has entered yes / no to a question
-def yes_no(question):
-
-    while True:
-        response = input(question).lower()
-
-        if response == "yes" or response == "y":
-            return "yes"
-            
-        elif response == "no" or response == "n":
-            return "no"
-            
-        else:
-            print("Please enter yes or no")
-            
+  
 # checks that user response is not blank
 def not_blank(question):
     
@@ -55,14 +41,39 @@ def calc_ticket_price(var_age):
         price = 6.5
         
     return price
-    
-# set maximum number of tickets below
 
+# checks that users enter a valid response (eg yes / no)
+# cash / credit) based on a list of options
+def string_checker(question, num_letters, valid_responses):
+    
+    error = "Please choose {} or {}".format(valid_responses[0], valid_responses[1])
+
+    if num_letters == 1:
+        short_version = 1
+    else:
+        short_version = 2
+    
+    
+    while True:
+        response = input(question).lower()
+
+        for item in valid_responses:
+            if response == item[:short_version] or response == item:
+                return item
+
+        print(error)
+
+
+
+# set maximum number of tickets below
 MAX_TICKETS = 3
 tickets_sold = 0
 
+yes_no_list = ["yes", "no"]
+payment_list = ["cash", "credit"]
+
 # Ask user if they want to see the imstructions
-want_instructions = yes_no("Do you want to read the instructions?")
+want_instructions = string_checker("Do you want to read the instructions (y/n): ",1, yes_no_list)
 
 if want_instructions == "yes":
     print("instructions go here")
@@ -88,8 +99,14 @@ while tickets_sold < MAX_TICKETS:
 
     else:        
         print("?? That looks like a typo, please try agian.") 
-        continue    
-    
+        continue  
+
+    # CALCULATE TICKET COST
+    ticket_cost = calc_ticket_price(age)
+
+    # get payment method
+    pay_method = string_checker("Choose a payment method (cash / credit): ",2, payment_list)
+
     tickets_sold += 1
     
     # calculate ticket cost
@@ -97,10 +114,8 @@ while tickets_sold < MAX_TICKETS:
     print("Age: {}, Ticket price: ${:.2f}".format(age, ticket_cost))
     
 #output number of tickets sold
-
 if tickets_sold == MAX_TICKETS:
     print("congratulations you have sold all the tickets")
-
 else:
     print("you have sold {} ticket/s.  There is {} tickets/s "
 "remaining".format(tickets_sold, MAX_TICKETS - tickets_sold))
